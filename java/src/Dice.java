16ad9271b3e5906eebd650d30b3cb13ab06808d1
Nigth <omnes.theo@gmail.com>
2021-04-24 21:59:50 +0200

    start Dice constructor

--- java/src/Dice.java
+++ java/src/Dice.java
@@ -1,7 +1,9 @@
 package java.src;
 
 public class Dice extends Throwable {
-    public Dice() {
-
+    public Dice(int value) {
+        if( 4 < value || value > 20 ) {
+            
+        }
     }
 }
