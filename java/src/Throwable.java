9b1089eeee4d7d529bd421dc0eff93828230fd86
Sarah <sschlegel@myges.fr>
2021-04-24 22:26:56 +0200

    Throwable getters & setters

--- java/src/Throwable.java
+++ java/src/Throwable.java
@@ -6,4 +6,19 @@ public abstract class Throwable {
     protected int nbFaces;
     protected ArrayList<Integer> faces;
 
+    public int getNbFaces() {
+        return nbFaces;
+    }
+
+    public ArrayList<Integer> getFaces() {
+        return faces;
+    }
+
+    public void setNbFaces(int nbFaces) {
+        this.nbFaces = nbFaces;
+    }
+
+    public void setFaces(ArrayList<Integer> faces) {
+        this.faces = faces;
+    }
 }
