9e4cf1ac3a32ba7bdeb9462d8f1008f11afcdf34
Sarah <sschlegel@myges.fr>
2021-04-24 22:33:54 +0200

    Coin construction

--- java/src/Coin.java
+++ java/src/Coin.java
@@ -1,8 +1,15 @@
 package java.src;
 
+import java.util.ArrayList;
+
 public class Coin extends Throwable{
     public Coin() {
         super();
+        super.setNbFaces(2);
+        ArrayList<Integer> faces = new ArrayList<Integer>();
+        faces.add(1);
+        faces.add(2);
+        super.setFaces(faces);
     }
 
     public static int throwCoin () {
