a5bdcb09f79957812e02bedfbad0782085152a6f
Nigth <omnes.theo@gmail.com>
2021-04-28 17:00:40 +0200

    test single dice 6

--- java/src/Throw.java
+++ java/src/Throw.java
@@ -1,6 +1,42 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Throw {
-    public Throw() {
+
+    private int nbDiceFaces[];
+    private int nbCoin;
+    private int nbKnucklebone;
+    private ArrayList<Integer> lastThrow;
+
+    public Throw(int nbDiceFaces[], int nbCoin, int nbKnucklebone) {
+        this.nbDiceFaces = nbDiceFaces;
+        this.nbCoin = nbCoin;
+        this.nbKnucklebone =  nbKnucklebone;
+        this.lastThrow = new ArrayList<>();
+        this.lastThrow.add(-1);
+    }
+
+    public ArrayList<Integer> run() {
+        ArrayList<Integer> results = new ArrayList<>();
+        for(int i = 0; i < nbDiceFaces.length; i++){
+            Dice d = new Dice(nbDiceFaces[i]);
+            results.add(d.roll());
+        }
+
+        Collections.sort(results);
+        Collections.reverse(results);
+
+        this.lastThrow.clear();
+        for(int i = 0; i < results.size(); i++)
+            lastThrow.add(results.get(i));
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        return this.lastThrow.toString();
     }
 }
--- java/test/DiceTest.java
+++ java/test/DiceTest.java
@@ -6,6 +6,9 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import src.Dice;
+import src.Throw;
+
+import java.util.ArrayList;
 
 
 public class DiceTest {
@@ -18,5 +21,12 @@ public class DiceTest {
     public void SingleDiceTest(){
         Dice d = new Dice(6);
         Assert.assertEquals(4, d.roll());
+    }
+
+    @Test
+    public void singleD6Roll() {
+        Throw throwTest = new Throw(new int[]{6}, 0,0);
+        throwTest.run();
+        Assert.assertEquals("[4]", throwTest.toString());
     }
 }
